4b94f3b321f0195d0db4c73bd0c1fefb62171197
Jacob Hardman <jacobhardmanbusiness@gmail.com>
2019-04-19 15:09:03 -0600

    Added basic game functions and basic sounds
    The game board now accurately tracks whose turn it is as well as updating the game board in order to reflect player moves. The basic soundscape has been imported but it needs more work.

--- Tic-Tac-Toe.py
+++ Tic-Tac-Toe.py
@@ -4,9 +4,8 @@
 # 4/10/19
 # Python Version 3.7.3
 
-# Credit for sound effects to Marianne Gagnon
-# http://soundbible.com/1682-Robot-Blip.html
-# http://soundbible.com/1669-Robot-Blip-2.html
+# Credit for punch sound effect to Mike Koenig: http://soundbible.com/995-Jab.html
+# All other sounds are in public domain
 
 # Importing pkgs
 import tkinter as tk
@@ -38,7 +37,7 @@ class HoverButton(tk.Button):
         self['background'] = self.defaultBackground
 
 # Creating a custom font for the program to use
-Text_Font = tkFont.Font(family='Helvetica', size=30, weight='bold')
+Text_Font = tkFont.Font(family='Helvetica', size=40, weight='bold')
 
 # Background image to store in the Title Frame
 Title_Background = tk.PhotoImage(file="Tic-Tac-Toe-Background.png")
@@ -49,8 +48,32 @@ Turn_Tracker = tk.StringVar()
 Turn_Tracker.set("X's Turn")
 
 # Tracks whether the the gameboard should display an 'X' or an 'O'
-Game_Piece = tk.StringVar()
-Game_Piece.set("")
+North_West_Text = tk.StringVar()
+North_West_Text.set("")
+
+North_Text = tk.StringVar()
+North_Text.set("")
+
+North_East_Text = tk.StringVar()
+North_East_Text.set("")
+
+West_Text = tk.StringVar()
+West_Text.set("")
+
+Center_Text = tk.StringVar()
+Center_Text.set("")
+
+East_Text = tk.StringVar()
+East_Text.set("")
+
+South_West_Text = tk.StringVar()
+South_West_Text.set("")
+
+South_Text = tk.StringVar()
+South_Text.set("")
+
+South_East_Text = tk.StringVar()
+South_East_Text.set("")
 
 ########################################################## PROGRAM LOGIC ################################################################
 
@@ -89,35 +112,185 @@ def Init_GUI():
 ### Initiating the active GUI elements
 def Active_GUI():
 
-    North_West = HoverButton(Gameboard, bg="black", bd="2")
+    North_West = HoverButton(Gameboard, bg="dark red", bd="2", fg="black", activebackground="red", activeforeground="black",
+    font=Text_Font, textvariable=North_West_Text, command=lambda:[Update_Grid(1)])
     North_West.place(relwidth="0.333", relheight="0.33")
 
-    North = HoverButton(Gameboard, bg="black", bd="2")
+    North = HoverButton(Gameboard, bg="dark red", bd="2", fg="black", activebackground="red", activeforeground="black",
+    font=Text_Font, textvariable=North_Text, command=lambda:[Update_Grid(2)])
     North.place(relx="0.333", relwidth="0.333", relheight="0.33")
 
-    North_East = HoverButton(Gameboard, bg="black", bd="2")
+    North_East = HoverButton(Gameboard, bg="dark red", bd="2", fg="black", activebackground="red", activeforeground="black",
+    font=Text_Font, textvariable=North_East_Text, command=lambda:[Update_Grid(3)])
     North_East.place(relx="0.6663", relwidth="0.3335", relheight="0.33")
 
-    West = HoverButton(Gameboard, bg="black", bd="2")
+    West = HoverButton(Gameboard, bg="dark red", bd="2", fg="black", activebackground="red", activeforeground="black",
+    font=Text_Font, textvariable=West_Text, command=lambda:[Update_Grid(4)])
     West.place(rely="0.33", relwidth="0.333", relheight="0.33")
 
-    Center = HoverButton(Gameboard, bg="black", bd="2")
+    Center = HoverButton(Gameboard, bg="dark red", bd="2", fg="black", activebackground="red", activeforeground="black",
+    font=Text_Font, textvariable=Center_Text, command=lambda:[Update_Grid(5)])
     Center.place(relx="0.333", rely="0.33", relwidth="0.333", relheight="0.33")
 
-    East = HoverButton(Gameboard, bg="black", bd="2")
+    East = HoverButton(Gameboard, bg="dark red", bd="2", fg="black", activebackground="red", activeforeground="black",
+    font=Text_Font, textvariable=East_Text, command=lambda:[Update_Grid(6)])
     East.place(relx="0.6663", rely="0.33", relwidth="0.3335", relheight="0.33")
 
-    South_West = HoverButton(Gameboard, bg="black", bd="2")
+    South_West = HoverButton(Gameboard, bg="dark red", bd="2", fg="black", activebackground="red", activeforeground="black",
+    font=Text_Font, textvariable=South_West_Text, command=lambda:[Update_Grid(7)])
     South_West.place(rely="0.66", relwidth="0.333", relheight="0.33")
 
-    South = HoverButton(Gameboard, bg="black", bd="2")
+    South = HoverButton(Gameboard, bg="dark red", bd="2", fg="black", activebackground="red", activeforeground="black",
+    font=Text_Font, textvariable=South_Text, command=lambda:[Update_Grid(8)])
     South.place(relx="0.333", rely="0.66", relwidth="0.333", relheight="0.33")
 
-    South_East = HoverButton(Gameboard, bg="black", bd="2")
+    South_East = HoverButton(Gameboard, bg="dark red", bd="2", fg="black", activebackground="red", activeforeground="black",
+    font=Text_Font, textvariable=South_East_Text, command=lambda:[Update_Grid(9)])
     South_East.place(relx="0.6663", rely="0.66", relwidth="0.3335", relheight="0.33")
 
-    Reset_Button = HoverButton(Reset, bg="red", font=Text_Font, text="Reset Game")
+    Reset_Button = HoverButton(Reset, bg="dark red", font=Text_Font, text="Reset Game", fg="black", activebackground="red", activeforeground="black",
+    command=lambda:Reset_Game())
     Reset_Button.place(relwidth="1", relheight="1")
+
+### Updates the display to show whose turn it is.
+def Update_Display():
+
+    if Turn_Tracker.get() == "X's Turn":
+        Turn_Tracker.set("O's Turn")
+    elif Turn_Tracker.get() == "O's Turn":
+        Turn_Tracker.set("X's Turn")
+    
+    print("Button was pushed")
+
+### Updates the gameboard to display the player's moves.
+def Update_Grid(arg):
+
+    if arg == 1:
+        if North_West_Text.get() == "":
+            if Turn_Tracker.get() == "X's Turn":
+                sound.PlaySound("Jab.wav", 1)
+                North_West_Text.set("X")
+                Update_Display()
+            else:
+                North_West_Text.set("O")
+                Update_Display()
+        else:
+            sound.PlaySound("A-Tone.wav", 1)
+            messagebox.showerror(title="ERROR", message="You cannot play on a place that has already been played on.")
+            
+    elif arg == 2:
+        if North_Text.get() == "":
+            if Turn_Tracker.get() == "X's Turn":
+                North_Text.set("X")
+                Update_Display()
+            else:
+                North_Text.set("O")
+                Update_Display()
+        else:
+            sound.PlaySound("A-Tone.wav", 1)
+            messagebox.showerror(title="ERROR", message="You cannot play on a place that has already been played on.")
+
+    elif arg == 3:
+        if North_East_Text.get() == "":
+            if Turn_Tracker.get() == "X's Turn":
+                North_East_Text.set("X")
+                Update_Display()
+            else:
+                North_East_Text.set("O")
+                Update_Display()
+        else:
+            sound.PlaySound("A-Tone.wav", 1)
+            messagebox.showerror(title="ERROR", message="You cannot play on a place that has already been played on.")
+
+    elif arg == 4:
+        if West_Text.get() == "":
+            if Turn_Tracker.get() == "X's Turn":
+                West_Text.set("X")
+                Update_Display()
+            else:
+                West_Text.set("O")
+                Update_Display()
+        else:
+            sound.PlaySound("A-Tone.wav", 1)
+            messagebox.showerror(title="ERROR", message="You cannot play on a place that has already been played on.")
+
+    elif arg == 5:
+        if Center_Text.get() == "":
+            if Turn_Tracker.get() == "X's Turn":
+                Center_Text.set("X")
+                Update_Display()
+            else:
+                Center_Text.set("O")
+                Update_Display()
+        else:
+            sound.PlaySound("A-Tone.wav", 1)
+            messagebox.showerror(title="ERROR", message="You cannot play on a place that has already been played on.")
+
+    elif arg == 6:
+        if East_Text.get() == "":
+            if Turn_Tracker.get() == "X's Turn":
+                East_Text.set("X")
+                Update_Display()
+            else:
+                East_Text.set("O")
+                Update_Display()
+        else:
+            sound.PlaySound("A-Tone.wav", 1)
+            messagebox.showerror(title="ERROR", message="You cannot play on a place that has already been played on.")
+
+    elif arg == 7:
+        if South_West_Text.get() == "":
+            if Turn_Tracker.get() == "X's Turn":
+                South_West_Text.set("X")
+                Update_Display()
+            else:
+                South_West_Text.set("O")
+                Update_Display()
+        else:
+            sound.PlaySound("A-Tone.wav", 1)
+            messagebox.showerror(title="ERROR", message="You cannot play on a place that has already been played on.")
+
+    elif arg == 8:
+        if South_Text.get() == "":
+            if Turn_Tracker.get() == "X's Turn":
+                South_Text.set("X")
+                Update_Display()
+            else:
+                South_Text.set("O")
+                Update_Display()
+        else:
+            sound.PlaySound("A-Tone.wav", 1)
+            messagebox.showerror(title="ERROR", message="You cannot play on a place that has already been played on.")
+
+    elif arg == 9:
+        if South_East_Text.get() == "":
+            if Turn_Tracker.get() == "X's Turn":
+                South_East_Text.set("X")
+                Update_Display()
+            else:
+                South_East_Text.set("O")
+                Update_Display()
+        else:
+            sound.PlaySound("A-Tone.wav", 1)
+            messagebox.showerror(title="ERROR", message="You cannot play on a place that has already been played on.")
+
+### Resets the gameboard and the turn tracker
+def Reset_Game():
+
+    
+
+    Turn_Tracker.set("X's Turn")
+    North_West_Text.set("")
+    North_Text.set("")
+    North_East_Text.set("")
+    West_Text.set("")
+    Center_Text.set("")
+    East_Text.set("")
+    South_West_Text.set("")
+    South_Text.set("")
+    South_East_Text.set("")
+
+    print("Reset button pushed")
 
 ########################################################### PROGRAM FLOW ################################################################
 
@@ -127,8 +300,10 @@ if __name__ == "__main__":
     if 'win' in sys.platform:
         ctypes.windll.shcore.SetProcessDpiAwareness(1)
 
-Init_GUI() # Initiating the GUI
-Active_GUI()
+Init_GUI() # Initiating the static GUI elements
+Active_GUI() # Initiating the active GUI elements
+
+sound.PlaySound("Gong.wav", sound.SND_ASYNC | sound.SND_NOSTOP)
 
 # Looping in the main window to accept User input
 Window.mainloop()
